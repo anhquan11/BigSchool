84a007aeb965c39022ad2365c51122a47bc52398
Administrator <Administrator@PC706>
2022-06-03 15:59:35 +0700

    Add project files.

---
+++ LabSchool_LeDangAnhQuan/Controllers/CoursesController.cs
@@ -0,0 +1,40 @@
+﻿using LabSchool_LeDangAnhQuan.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabSchool_LeDangAnhQuan.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                Datetime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ LabSchool_LeDangAnhQuan/Models/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabSchool_LeDangAnhQuan.Models
+{
+    public class Course
+    {
+        internal byte CategoryId;
+
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime Datetime { get; set; }
+    }
+    public class CourseViewModel
+{       [Required]
+        public string Place { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+    }
+}
